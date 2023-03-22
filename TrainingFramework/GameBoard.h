85b6dca405d687023daa7d4b0272f3f91cb4e8ce
CuO <legendof2k2@gmail.com>
2023-03-22 11:50:48 +0700

    [CuO] add Class GameBoard Entity Player

---
+++ TrainingFramework/GameBoard.h
@@ -0,0 +1,17 @@
+#pragma once
+#include"Sprite2D.h"
+class GameBoard 
+{
+private:
+	std::vector < std::vector<Sprite2D>> m_gameBoard;
+public:
+	GameBoard();
+	~GameBoard();
+	void generateGameBoard(); //generate a new game board 
+	bool hasAnAvailableMove(); //check if there are any available move
+	bool hasAnMatch(); //Check if there are at least 3 pieces matching together
+	bool canSwapTwoPiece();  //check if the swap is available by check the same neighbor pieces 
+	void swapTwoPiece();  //Swap 2 piece
+	void destroyPieces(std::vector<std::vector<int>>); //Destroy pieces that match together
+	void refillGameBoard(); //Refill Gameboard after destroy;
+};
