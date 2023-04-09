fdeb6ee8f6f209f7f6b96cc8902489e97e670ec4
DuyTien <ddtien0909@gmail.com>
2023-04-09 21:33:53 +0700

    add Level, GSLevel and Save

---
+++ TrainingFramework/src/GameManager/SaveData.cpp
@@ -0,0 +1,65 @@
+#include "SaveData.h"
+#include <iostream>
+
+SaveData::SaveData()
+{
+	m_DataPath = "..\\Data\\SaveGame\\";
+}
+
+SaveData::~SaveData()
+{
+
+}
+
+bool SaveData::IsNewGame()
+{
+	if (std::filesystem::is_empty(m_DataPath))
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+void SaveData::ClearDataFolder()
+{
+	for (auto& entry : std::filesystem::directory_iterator(m_DataPath))
+		std::filesystem::remove_all(entry.path());
+	
+	std::cout << "Clear Data!" << std::endl;
+}
+
+void SaveData::SaveLevel(int numPassedlevel)
+{
+	FILE* fp = NULL;
+	std::string filePath = m_DataPath + "Level.txt";
+	fp = fopen(filePath.c_str(), "w");
+
+	if (fp != NULL)
+	{
+		fprintf(fp, "%s %d", "Level Passed:", numPassedlevel);
+		fclose(fp);
+	}
+	
+	
+}
+
+int SaveData::LoadLevel()
+{
+	FILE* fp = NULL;
+	std::string filePath = m_DataPath + "Level.txt";
+	fp = fopen(filePath.c_str(), "r");
+	int numPassedLevel = 0;
+	char sTmp[10];
+	if (fp == NULL)
+		return -1;
+	else
+	{
+		fscanf(fp, "%s %d", sTmp, &numPassedLevel);
+		fclose(fp);
+		std::printf("%d\n", numPassedLevel);
+	}
+	return numPassedLevel;
+}
